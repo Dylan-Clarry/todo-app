7da6ba2bdb51a1cd50d9d33c577767726da3a553
Dylan <dylan.imail@gmail.com>
2022-08-21 21:34:16 -0400

    Form created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Form from './components/Form';
 
 const App = () => {
     return(
         <h1>
             Hello World!
+            <Form/>
         </h1>
     );
 };
---
+++ src/components/Form.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Form = () => {
+    return(
+        <form>
+            <input
+                type="text"
+                className="form-input"
+            />
+            <button
+                type="submit"
+            >
+            +</button>
+        </form>
+    );
+};
+
+export default Form;
