6d4b889f81e90ca01a326454d4d35c2730489839
Dylan <dylan.imail@gmail.com>
2022-09-12 21:17:44 -0400

    added Typescript to project; Babel ripbozo

--- index.tsx
+++ index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./src/App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
-const container = document.getElementById("root");
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 root.render(
     <React.StrictMode>
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,47 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: './index.js',
-  mode: 'development',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'index_bundle.js',
-  },
-  target: 'web',
-  devServer: {
-    port: '5000',
-    static: {
-      directory: path.join(__dirname, 'public')
-},
-    open: true,
-    hot: true,
-    liveReload: true,
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/, 
-        exclude: /node_modules/, 
-        use: 'babel-loader', 
-      },
-        {
-            test: /\.(css|scss)$/,
-            use: ["style-loader", "css-loader", "sass-loader"]
+    entry: "./index.tsx",
+    mode: "development",
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "index_bundle.js",
+    },
+    target: "web",
+    devServer: {
+        port: "5000",
+        static: {
+            directory: path.join(__dirname, "public"),
         },
+        open: true,
+        hot: true,
+        liveReload: true,
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(t|j)sx?$/,
+                exclude: /node_modules/,
+                use: { loader: "ts-loader" },
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader",
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "public", "index.html"),
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html')
-    })
-  ]
 };
