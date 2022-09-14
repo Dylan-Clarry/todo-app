90e9c23aa88bade1392820e08b1659361927ac0e
Dylan <dylan.imail@gmail.com>
2022-09-14 19:56:52 -0400

    DragBoard works but also doesn't???

--- src/components/ui/DragBoard.tsx
+++ src/components/ui/DragBoard.tsx
@@ -1,27 +1,55 @@
 import React from "react";
 import "./DragBoard.style.scss";
 
+const DragContainer = (props: any) => {
+  const handleDragOver = (e: any) => {
+    console.log("dragoon", e.target);
+    e.preventDefault();
+    const draggable = document.querySelector(".dragging");
+    e.target.appendChild(draggable);
+  };
+
+  return (
+    <div className="dragboard__container" onDragOver={handleDragOver(this)}>
+      {props.children}
+    </div>
+  );
+};
+
+const Draggable = (props: any) => {
+  const handleDragStart = (e: any) => {
+    e.target.classList.add("dragging");
+  };
+
+  const handleDragEnd = (e: any) => {
+    e.target.classList.remove("dragging");
+  };
+
+  return (
+    <p
+      className="draggable"
+      draggable="true"
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+    >
+      {props.children}
+    </p>
+  );
+};
+
 const DragBoard = () => {
-    return (
-        <div className="dragboard">
-            <div className="dragboard__container">
-                <p className="draggable" draggable="true">
-                    1
-                </p>
-                <p className="draggable" draggable="true">
-                    2
-                </p>
-            </div>
-            <div className="dragboard__container">
-                <p className="draggable" draggable="true">
-                    3
-                </p>
-                <p className="draggable" draggable="true">
-                    5
-                </p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="dragboard">
+      <DragContainer>
+        <Draggable>1</Draggable>
+        <Draggable>2</Draggable>
+      </DragContainer>
+      <DragContainer>
+        <Draggable>3</Draggable>
+        <Draggable>4</Draggable>
+      </DragContainer>
+    </div>
+  );
 };
 
 export default DragBoard;
